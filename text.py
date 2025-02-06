ab1b92ae85bac09030ed373d33bece355d681cdf
hamzah377 <1004048@pdsb.net>
2025-02-06 12:28:33 -0500

    Create text.py

---
+++ text.py
@@ -0,0 +1,7 @@
+print("There are ", 67, " video games.");
+print("your team lost by: ", (6.7+6.7))
+print("There are %d people in this room." % 67)
+print("pi is equal to %f" % 6.723)
+print("%s's final score is %d" % ("hamzah", 67))
+print("Total money spent is: %.23f" % (67+23+21))
+print("The area of a circle of radius %d is %f" % (4, 3.1416*4*4))
