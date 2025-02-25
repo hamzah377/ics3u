79d6186da70a9fb0677e3c069b07b14c68cdab8b
hamzah377 <1004048@pdsb.net>
2025-02-24 21:17:25 -0500

    Create lesson 7.py

---
+++ lesson 7.py
@@ -0,0 +1,10 @@
+year = input("Year of birth: ")   
+age = input("Age: ")                
+dbyear =   year+year   
+add= dbyear+5
+year_result = add*50
+year_age= year_result+age
+minus= year_age-250
+divide_year=minus/100
+divide_year=minus/100
+print("Complete Formula is",divide_year)
