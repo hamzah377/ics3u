8e11d992e0afbbc96a2919b7222bf7b148e3ccb5
hamzah377 <1004048@pdsb.net>
2025-03-24 12:13:30 -0400

    Update assignment 2.py

--- assignment 2.py
+++ assignment 2.py
@@ -1,18 +1,13 @@
 import random
 
-a = random.randrange(1,100) 
-print("Hello! Welcome to the number guessing game!")
-print("I am thinking of a number between 1 and 100. It is your turn to guess what it is. You have a maximum of six (6) tries.")
-c = 1 
-x = 0
-while c < 7 and x != a:
-    x = int(input("Guess #" + str(c) + ": "))
-    if x < a:
-       print("Higher")
-    if x > a:
-       print("Lower")
+a = random.randint(1, 100)
+print("Guess the number (1-100)! You have 6 tries.")
+
+for c in range(6):
+    x = int(input("Your guess: "))
     if x == a:
-       print("You Guessed right!")
-    c += 1
-if c >= 7 and x != a:
-    print("you are out of guesses! The answer was" , a , ". Better luck next time!")
+        print("Correct! ")
+        
+    print("Higher" if x < a else "Lower")
+else:
+    print(f"Out of tries! The number was {a}.")
